b8d1d127d3a21e15f6e819b29f59a0acd34b8c44
jahu00 <jahu00@users.noreply.github.com>
2015-02-23 23:26:19 +0100

    Small cleanup

--- www/js/getCharacterAtPoint.js
+++ www/js/getCharacterAtPoint.js
@@ -1,7 +1,7 @@
 getCharacterAtPoint = {
 	find: function(element, x, y)
 	{
-		//var lastBottom = 0;
+		//Returns text node and character position based on provided coordinates
 		for (var i = 0; i < element.childNodes.length; i++)
 		{
 			var node = element.childNodes[i];
@@ -12,7 +12,6 @@ getCharacterAtPoint = {
 				{
 					continue;
 				}
-				var compensation = 0;
 				var firstPosition = node.nodeValue.indexOf(trim[0]);
 				var lastPosition = firstPosition + trim.length;
 				var firstCharacter = this.getBoundingBox(node, firstPosition);
@@ -43,7 +42,7 @@ getCharacterAtPoint = {
 							if(
 								boundingBox.left <= x && boundingBox.right >= x &&
 								boundingBox.top  <= y && boundingBox.bottom >= y) {
-
+								
 								range.detach();
 								return { "node": node, "position": currentPosition };
 							}
@@ -69,7 +68,7 @@ getCharacterAtPoint = {
 		range.detach();
 		//return this.editableBoundingBox(boundingBox);
 		return boundingBox;
-	},
+	}/*,
 	editableBoundingBox: function(boundingBox)
 	{
 		return {
@@ -82,5 +81,5 @@ getCharacterAtPoint = {
 			x: boundingBox.x,
 			y: boundingBox.y
 		};
-	}
+	}*/
 }
